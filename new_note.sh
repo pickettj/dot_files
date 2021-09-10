8cb71c8c2106369363404732eb2f2ad4b477cfc8
pickettj <pickettj@gmail.com>
2021-09-10 17:24:08 -0400

    failsafe

--- new_note.sh
+++ new_note.sh
@@ -1,4 +1,4 @@
-# Trying to make a simple script that creates and opens a file
+  # Trying to make a simple script that creates and opens a file
 #!/usr/bin/env bash
 read -p "Note title: " note
 touch ~/Dropbox/Active_Directories/Notes/Secondary_Works/$note".txt"
